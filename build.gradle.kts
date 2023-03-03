1bef08d06d575243f5eac49f92277c70eccfee28
jakebae87 <jeonghyun.bae@kakao.com>
2023-03-03 23:16:30 +0900

    첫 커밋

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.9"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "com.jake"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+//	testImplementation("org.springframework.security:spring-security-test")
+
+	implementation ("org.apache.tomcat.embed:tomcat-embed-jasper")
+	implementation ("javax.servlet:jstl")
+//	implementation ("org.springframework.security:spring-security-taglibs")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
